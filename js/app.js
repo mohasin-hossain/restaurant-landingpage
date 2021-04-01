e52a36cc5848734912ec37d68002ddaf46e44d0f
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-04-01 21:10:06 +0600

    Responsive Design

--- js/app.js
+++ js/app.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: ".prev-btn",
-        nextArrow: ".next-btn"
+        nextArrow: ".next-btn",
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
+
+    // Mobile nav toggle
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+    })
 });
+
+
